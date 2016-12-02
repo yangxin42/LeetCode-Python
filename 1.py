c2e1f1d447bd3ab8b7d5c9dc5fe32acbc5841352
yangxin42 <progress.tmp@gmail.com>
2016-12-02 10:09:19 +0800

    Create 1.py
    https://leetcode.com/problems/two-sum/

---
+++ 1.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        for i in range(len(nums)):
+            for j in range(n):
+                if i != j and target - nums[i] == nums[j]:
+                    l = []
+                    l.append(i)
+                    l.append(j)
+                    return l
